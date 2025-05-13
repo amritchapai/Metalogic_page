b8428b2b12e42c43db91f23f8fa9f104e7911073
amritchapai <chapaiamrit@gmail.com>
2025-05-13 20:59:36 +0545

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { ChevronDown } from "lucide-react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="fixed z-10 top-0 w-full h-24 flex justify-between items-center bg-[#021642] text-white px-16 py-3 opacity-80">
+      <div className="flex items-center">
+        <div className="bg-[#e83d3d] w-10 h-10 rounded-full flex justify-center items-center mr-2">
+          <img src="metalogo.png" />
+        </div>
+        <span className="text-3xl font-bold tracking-wider">MetaLogic</span>
+      </div>
+
+      <ul className="flex space-x-8 text-xl">
+        <li>
+          <a href="#" className="font-medium">
+            Home
+          </a>
+        </li>
+        <li className="relative">
+          <button className="flex items-center font-medium">
+            Services
+            <ChevronDown className="ml-1 w-4 h-4" />
+          </button>
+        </li>
+        <li>
+          <a href="#" className="font-medium">
+            Career
+          </a>
+        </li>
+        <li>
+          <a href="#" className="font-medium">
+            Blogs
+          </a>
+        </li>
+        <li>
+          <a href="#" className="px-2 font-medium border-b-4 border-[#e83d3d]">
+            About Us
+          </a>
+        </li>
+      </ul>
+
+      <button className="bg-[#e83d3d] hover:bg-red-600 text-white font-medium px-4 py-2 rounded ">
+        Get in Touch
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
